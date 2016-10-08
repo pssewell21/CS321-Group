d8f7ff1b4a6423eec1f2895f3862298404ea61d6
Patrick Sewell <pssewell21@gmail.com>
2016-10-08 14:48:58 -0500

    even more library work

--- TeamOneProject/src/Library/LibraryFactoryBase.java
+++ TeamOneProject/src/Library/LibraryFactoryBase.java
@@ -10,6 +10,9 @@ import UI.Common.ExceptionHandler;
 import UI.Common.IdGenerator;
 import UI.Common.Utility;
 import java.math.BigInteger;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 
@@ -34,7 +37,26 @@ public abstract class LibraryFactoryBase implements ISqlGenerator
         
         try
         {
-            DataAccessJavaDb.executeSelect(generateSelect(hashMap));
+            ResultSet resultSet = DataAccessJavaDb.executeSelect(generateSelect(hashMap));
+            ResultSetMetaData metaData = resultSet.getMetaData();
+        
+            resultSet.            
+            for (int i = 1; i <= metaData.getColumnCount(); i++)
+            {
+                System.out.printf("%-10s\t", metaData.getColumnName(i));
+            }
+            
+            System.out.println();
+           
+            while (resultSet.next())
+            {
+                for (int i = 1; i <= metaData.getColumnCount(); i++)
+                {
+                    System.out.printf("%-10s\t", resultSet.getObject(i));
+                }
+                
+                System.out.println();
+            }
         }
         catch (Exception e)
         {
