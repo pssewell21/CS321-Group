89d8e18f37e4333f40e572d11a4e096439aa3e4a
pssewell21 <pssewell21@gmail.com>
2016-10-30 09:04:03 -0500

    Stubbing in EditViewControllerBase

---
+++ TeamOneProject/src/Controllers/EditViewControllerBase.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+/**
+ *
+ * @author Owner
+ */
+public abstract class EditViewControllerBase {
+    
+    // <editor-fold defaultstate="collapsed" desc="Member Variables"> 
+    
+//    public TestData model;
+//    public TestDataEditView view;
+//
+//    public boolean isNew;
+//
+//    TestDataFactory factory;
+    
+    // </editor-fold> 
+}
